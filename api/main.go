3e467303d205ad909b50e4136647f695eb857c31
Siddiga Gadirova <siddiga.gadirova@technofusion.az>
2024-03-28 15:22:18 +0400

    -- request and response structs and routes added

--- api/main.go
+++ api/main.go
@@ -1,1 +1,15 @@
-package api
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"url-shortener-golang-redis/routes"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/v1", routes.ShortenURL)
+}
+
+func main() {
+
+}
